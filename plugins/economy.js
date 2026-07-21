401744a6d1f12e8f7207d09e9d1bce91a5f78bcd
Mulax Prime <161124347+mulaxprime@users.noreply.github.com>
2026-07-21 21:09:06 +0200

    Refactor economy.js to remove whitespace and newlines

--- plugins/economy.js
+++ plugins/economy.js
@@ -1,9 +1,1 @@
-module.exports = {
-    name: 'economy',
-    version: '1.0.0',
-    users: {},
-    getBalance: (userId) => 0,
-    addBalance: (userId, amount) => amount,
-    removeBalance: (userId, amount) => true,
-    transfer: (from, to, amount) => true
-};
+module.exports={name:'economy',version:'1.0.0',users:{},getBalance:(userId)=>0,addBalance:(userId,amount)=>amount,removeBalance:(userId,amount)=>true,transfer:(from,to,amount)=>true};
